8b0947d3ee8e6a05663ae2a3280cf0c1fcce813b
ghsable <ghsable@users.noreply.github.com>
2021-12-01 13:53:10 +0000

    deploy: 932db4e3dc89db9da9833f2bcc4e474504bba838

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"mousemode":{"doc":"Overview","t":[5],"n":["main"],"q":["mousemode"],"d":["Entry Point"],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["mousemode"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
