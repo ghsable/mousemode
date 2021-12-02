b7068921e8895e886d30d6a7782cbfc9742e9c94
suna <sn.sable005@gmail.com>
2021-12-02 13:17:27 +0900

    main.rs: Update doc

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 //! Overview
 //!
-//! mousemode is a daemon that let us to use mouse operations on Keyboard.
+//! mousemode is a daemon that let us to use mouse operations on the Keyboard.
 use std::{thread, time};
 use device_query::{DeviceQuery, DeviceState};
 use mouse_rs::{types::keys::Keys, Mouse};
 
 /// Entry Point
 ///
-/// This function converts a keyboard event to a mouse event.
+/// This function converts keyboard events to mouse events.
 fn main() {
     let device_state = DeviceState::new();
     let mouse = Mouse::new();
@@ -24,17 +24,17 @@ fn main() {
         let keys_0 = keys[0].to_string();
         let keys_1 = keys[1].to_string();
 
-        //mouse operation: ↑, ↓, ←, →
+        // ←, ↑, ↓, →
+        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouses.coords.0 - 20, mouses.coords.1).expect("Unable to move mouse"); }
         if keys_0 == "Key2" && keys_1 == "P" { mouse.move_to(mouses.coords.0, mouses.coords.1 - 20).expect("Unable to move mouse"); }
         if keys_0 == "Key3" && keys_1 == "P" { mouse.move_to(mouses.coords.0, mouses.coords.1 + 20).expect("Unable to move mouse"); }
-        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouses.coords.0 - 20, mouses.coords.1).expect("Unable to move mouse"); }
         if keys_0 == "Key4" && keys_1 == "P" { mouse.move_to(mouses.coords.0 + 20, mouses.coords.1).expect("Unable to move mouse"); }
-        //mouse operation: left-click
+        // left-click
         if keys_0 == "W" && keys_1 == "P" {
             mouse.press(&Keys::LEFT).expect("Unable to press button");
             mouse.release(&Keys::LEFT).expect("Unable to press button");
         }
-        //mouse operation: right-click
+        // right-click
         if keys_0 == "Q" && keys_1 == "P" {
             mouse.press(&Keys::RIGHT).expect("Unable to press button");
             mouse.release(&Keys::RIGHT).expect("Unable to press button");
