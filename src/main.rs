a6cf6211e17e7b7b4bf1dad2ccca8d824a9af455
ghsable <sn.sable005@gmail.com>
2021-12-07 22:33:47 +0900

    move mouse_state

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,20 @@ fn main() {
     loop {
         thread::sleep(time::Duration::from_millis(100));
 
-        let keys = device_state.get_keys();
-        let mouses = device_state.get_mouse();
-        //println!("{:?}", keys);
+        let keys_state = device_state.get_keys();
+        println!("{:?}", keys_state);
 
-        if keys.len() != 2 { continue; }
+        if keys_state.len() != 2 { continue; }
 
-        let keys_0 = keys[0].to_string();
-        let keys_1 = keys[1].to_string();
+        let keys_0 = keys_state[0].to_string();
+        let keys_1 = keys_state[1].to_string();
+        let mouse_state = device_state.get_mouse();
 
         // ←, ↑, ↓, →
-        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouses.coords.0 - 20, mouses.coords.1).expect("Unable to move mouse"); }
-        if keys_0 == "Key2" && keys_1 == "P" { mouse.move_to(mouses.coords.0, mouses.coords.1 - 20).expect("Unable to move mouse"); }
-        if keys_0 == "Key3" && keys_1 == "P" { mouse.move_to(mouses.coords.0, mouses.coords.1 + 20).expect("Unable to move mouse"); }
-        if keys_0 == "Key4" && keys_1 == "P" { mouse.move_to(mouses.coords.0 + 20, mouses.coords.1).expect("Unable to move mouse"); }
+        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 - 20, mouse_state.coords.1).expect("Unable to move mouse"); }
+        if keys_0 == "Key2" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 - 20).expect("Unable to move mouse"); }
+        if keys_0 == "Key3" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 + 20).expect("Unable to move mouse"); }
+        if keys_0 == "Key4" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 + 20, mouse_state.coords.1).expect("Unable to move mouse"); }
         // right-click
         if keys_0 == "Q" && keys_1 == "P" {
             mouse.press(&Keys::RIGHT).expect("Unable to press button");
