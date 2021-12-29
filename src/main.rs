9fb32675e78867f64b273420bf9ed3d8ba2ddc1e
ghsable <sn.sable005@gmail.com>
2021-12-29 14:28:31 +0900

    update mouse.move_to 20 to 15

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,10 @@ fn main() {
         let mouse_state = device_state.get_mouse();
 
         // ←, ↑, ↓, →
-        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 - 20, mouse_state.coords.1).expect("Unable to move mouse"); }
-        if keys_0 == "Key2" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 - 20).expect("Unable to move mouse"); }
-        if keys_0 == "Key3" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 + 20).expect("Unable to move mouse"); }
-        if keys_0 == "Key4" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 + 20, mouse_state.coords.1).expect("Unable to move mouse"); }
+        if keys_0 == "Key1" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 - 15, mouse_state.coords.1).expect("Unable to move mouse"); }
+        if keys_0 == "Key2" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 - 15).expect("Unable to move mouse"); }
+        if keys_0 == "Key3" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0, mouse_state.coords.1 + 15).expect("Unable to move mouse"); }
+        if keys_0 == "Key4" && keys_1 == "P" { mouse.move_to(mouse_state.coords.0 + 15, mouse_state.coords.1).expect("Unable to move mouse"); }
         // right-click
         if keys_0 == "Q" && keys_1 == "P" {
             mouse.press(&Keys::RIGHT).expect("Unable to press button");
